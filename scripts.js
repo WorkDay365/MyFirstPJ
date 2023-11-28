35f8c6620f0736f3b3407453699d69780a19abc9
AlexDev2112 <AlexDev2112@gmail.com>
2023-11-28 14:25:25 +0100

    Js + React

--- scripts.js
+++ scripts.js
@@ -1,90 +1,91 @@
-'use strict';
-let numberOfFilms;
-let genres =[];
-function start(){
-   //let numberOfFilms; //= +prompt('How many movies have you watched? ','');
-   
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = +prompt('How many movies have you watched? ','');
-   }
-}
+"use strict";
 
-
-start();
-
+let genres = [];
+alert(+"thousand");
+let gg = +"Thousand";
+alert(typeof gg);
 let personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-}
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  start: function () {
+    personalMovieDB.count = +prompt("How many movies have you watched? ", "");
 
-
-
-let endFilm; 
-let evaluation;
-
-function  rememberMyFilms() {
-   for (let i=1;i<2;i++){
-      const a = prompt('One the last film: ',''),
-            b = prompt('How do eveluation it? ','');
-      if(a !=null && b !=null && a != '' && b != '' && a.length < 50 ){
-         personalMovieDB.movies[a] = b;
-         console.log('done');
+    while (
+      personalMovieDB.count == "" ||
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count)
+    ) {
+      personalMovieDB.count = +prompt("How many movies have you watched? ", "");
+    }
+  },
+  rememberMyFilms: function () {
+    for (let i = 1; i < 2; i++) {
+      const a = prompt("One the last film: ", ""),
+        b = prompt("How do eveluation it? ", "");
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+      } else {
+        i--;
+        console.log("error");
       }
-      else{
-         i--;
-         console.log('error');
+    }
+  },
+  detectedPersonalLevel: function (params) {
+    if (personalMovieDB.count < 10) {
+      console.log("Do you like2 films?");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      console.log("You classic spectator");
+    } else if (personalMovieDB.count > 30) {
+      console.log("You is moviegoer?");
+    } else {
+      console.log("Error");
+    }
+  },
+
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i < 4; i++) {
+      let genre = prompt(`What are your like genre on namber  ${i} ?`, ``);
+      if (genre === "" || genre == null) {
+        console.log("You enter uncorrect data...");
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
       }
-      
-   } 
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Favorite ganre ${i + 1} - is this ${item}`);
+    });
+  },
+};
 
-}
+// let endFilm;
+// let evaluation;
 
-function detectedPersonalLevel(params) {
-   
-if (personalMovieDB.count<10){
-   console.log('Do you like2 films?');
-}
-else if (personalMovieDB.count>=10 && personalMovieDB.count<=30) {
-      console.log('You classic spectator');
-   }
- else if (personalMovieDB.count>30) {
-      console.log('You is moviegoer?');
-   }
-   else{
-      console.log('Error');
-   }
-}
+// showMyDB(personalMovieDB.privat);
 
-function showMyDB(hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB); 
-   }
-}
+// rememberMyFilms();
 
-function writeYourGenres() {
-   for (let i=1; i<4;i++) {
-        
-      genres.push(prompt(`What are your like genre on namber  ${i} ?`,``));
-   }
-}
+// detectedPersonalLevel();
 
-showMyDB(personalMovieDB.privat);
-
-
-
-
-rememberMyFilms();
-
-detectedPersonalLevel();
-
-writeYourGenres() ;
-console.log(genres); 
+// writeYourGenres();
+// console.log(genres);
 //console.log(personalMovieDB);
-
-
 
 /* 
 // Пишем решение вот тут
@@ -119,7 +120,7 @@ let i=1;
             console.log(i);
         }
      */
- /* }
+/* }
  console.log('5 ---------------------');
 
  const arrayOfNumbers = [];
@@ -153,7 +154,7 @@ let i=1;
      }
    
  }   */
- //console.log(data);
+//console.log(data);
 /*  console.log('3 ---------------------');
  
  const data = [5, 10, 'Shopping', 20, 'Homework'];
@@ -166,7 +167,7 @@ let i=1;
     IndTwo--
  }
  console.log(result); */
- /* console.log('1 ---------------------');
+/* console.log('1 ---------------------');
  
  const lines = 5;
  let result = '';
